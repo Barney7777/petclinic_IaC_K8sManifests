6efb9021e453d57e13e3751b4f61387a8020408b
Barney Wang <wangyaxu7@gmail.com>
2024-07-16 13:57:23 +1000

    update install.sh file

--- Jenkins-Server-TF/install-tools.sh
+++ Jenkins-Server-TF/install-tools.sh
@@ -17,4 +17,4 @@ sudo systemctl start jenkins
 # Install Packer
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install packer
+sudo apt-get update && sudo apt-get install -y packer
